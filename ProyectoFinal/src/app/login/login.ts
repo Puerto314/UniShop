a8344956dd8540b1b82782dab7ba717c7de5c470
Esteban <estebanpuertorojas235777@gmail.com>
2026-05-24 10:29:14 -0500

    Prueba 1
    Prueba 1

--- ProyectoFinal/src/app/login/login.ts
+++ ProyectoFinal/src/app/login/login.ts
@@ -152,7 +152,7 @@ export class Login {
       this.registerError = 'Las contraseñas no coinciden';
       return;
     }
- 
+
     if (!this.dominioPermitido(this.regEmail)) {
       this.registerError = 'Solo se permiten correos @gmail.com, @hotmail.com o @unbosque.edu.co';
       return;
